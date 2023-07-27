54d781164a87538d479136e1dcda90abbddc9b01
Xavier Beatrice <Xavbeat03@gmail.com>
2023-07-27 14:11:35 -0400

    WIP 2.1, continued on SQL stuff.

--- src/main/java/com/github/Xavbeat03/Tariffs/DataManagement/DB.java
+++ src/main/java/com/github/Xavbeat03/Tariffs/DataManagement/DB.java
@@ -7,6 +7,9 @@ import org.jetbrains.annotations.NotNull;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Borrowed from DB.java on AlathraWar in the alathra repository
+ */
 public abstract class DB {
 	public static @NotNull Connection get() throws SQLException {
 		return Main.getInstance().getDataManager().getConnection();
