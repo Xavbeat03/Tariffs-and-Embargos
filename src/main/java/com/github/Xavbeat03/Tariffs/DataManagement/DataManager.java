c2d20221d4e825e036bcc42d2f02a707276c7839
xavier <Xavbeat03@gmail.com>
2023-07-26 20:02:27 -0400

    WIP commit, still errors that exist

---
+++ src/main/java/com/github/Xavbeat03/Tariffs/DataManagement/DataManager.java
@@ -0,0 +1,15 @@
+package com.github.Xavbeat03.Tariffs.DataManagement;
+
+import com.github.Xavbeat03.Tariffs.Main;
+
+public class DataManager {
+    private final Main instance;
+
+    public DataManager(Main instance){
+        this.instance = instance;
+    }
+
+
+
+
+}
--- src/main/java/com/github/Xavbeat03/Tariffs/Handlers/SQLHandler.java
+++ src/main/java/com/github/Xavbeat03/Tariffs/Handlers/SQLHandler.java
@@ -1,5 +1,43 @@
 package com.github.Xavbeat03.Tariffs.Handlers;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SQLHandler {
+    public static void initDB(){
+        try(final Statement statement = DB.get().createStatement()){
+            statement.addBatch("""
+                CREATE TABLE IF NOT EXISTS `Town_Tariffs` (
+                `origin` uuid NOT NULL DEFAULT uuid(), 
+                `target` uuid NOT NULL DEFAULT uuid(),
+                `target_type` tinytext NOT NULL,
+                `value` int NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+                """);
+            statement.addBatch("""
+                CREATE TABLE IF NOT EXISTS `Player_Tariffs` (
+                `origin` uuid NOT NULL DEFAULT uuid(), 
+                `target` uuid NOT NULL DEFAULT uuid(),
+                `target_type` tinytext NOT NULL,
+                `value` int NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+                """);
+            statement.addBatch("""
+                CREATE TABLE IF NOT EXISTS `Nation_Tariffs` (
+                `origin` uuid NOT NULL DEFAULT uuid(), 
+                `target` uuid NOT NULL DEFAULT uuid(),
+                `target_type` tinytext NOT NULL,
+                `value` int NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+                """);
+            statement.executeLargeBatch();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        public static void SaveAll(){
+
+        }
+    }
 
 }
