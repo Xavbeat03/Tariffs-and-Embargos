d348be06337b67bc7cd61601d5b4c47b03f4200b
Xavier Beatrice <Xavbeat03@gmail.com>
2023-08-02 16:35:51 -0400

    Working on embargo command work.

--- src/main/java/com/github/Xavbeat03/Tariffs/command/Tariff.java
+++ src/main/java/com/github/Xavbeat03/Tariffs/command/Tariff.java
@@ -19,6 +19,7 @@ import org.bukkit.entity.Player;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Tariff {
     final TownyAPI townyInstance = Main.getInstance().getTownyInstance();
@@ -30,10 +31,11 @@ public class Tariff {
             .withArguments(
                     new StringArgument("t_entity_value" /* p, n, t*/).includeSuggestions(ArgumentSuggestions.strings("p", "n", "t")),
                     new StringArgument("t_tariffable_entity").includeSuggestions(ArgumentSuggestions.strings(String.valueOf(townyInstance.getTowns()), String.valueOf(townyInstance.getNations()))),
-                    new IntegerRangeArgument("t_tariff_value")
+                    new IntegerRangeArgument("t_tariff_value").
             )
             .executes(this::tariff)
             .register();
+
         new CommandAPICommand("ntariff")
                 .withFullDescription("This command allows your nation to tariff either a player, town, or nation with /tariff")
                 .withShortDescription("This command allows your nation to tariff either a player, town, or nation with /tariff")
@@ -45,6 +47,17 @@ public class Tariff {
                 )
                 .executes(this::ntariff)
                 .register();
+
+        new CommandAPICommand("embargo")
+            .withFullDescription("This command allows your town to embargo either a player, town, or nation with /embargo")
+            .withShortDescription("This command embargos other entities")
+            .withPermission("Tariffs.tariff")
+            .withArguments(
+                new StringArgument("t_entity_value").includeSuggestions(ArgumentSuggestions.strings("p", "n", "t")),
+                new StringArgument("t_tariffable_entity").includeSuggestions(ArgumentSuggestions.strings(String.valueOf(townyInstance.getTowns()), String.valueOf(townyInstance.getNations())))
+            )
+            .executes()
+            .register();
     }
 
     private void tariff(CommandSender sender, CommandArguments args) {
@@ -68,19 +81,25 @@ public class Tariff {
                 case "p" -> {
                     //check that arg 1 is a player, if so add it to the town tariff database
                     townyInstance.getTownyObjectStartingWith(tariffable_entity, "r");
-                    if(Bukkit.getPlayerUniqueId(tariffable_entity)==null) return; //TODO give player message that states player didn't exist
+                    if(Bukkit.getPlayerUniqueId(tariffable_entity)==null) return; //TODO give command sender message that states player didn't exist
                     TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), Bukkit.getPlayerUniqueId(tariffable_entity), arg_entity_value, tariff_value);
 
-                    SQLHandler.playerCreateTariff(DB.get(), tariff);
+                    SQLHandler.townCreateTariff(DB.get(), tariff);
                 }
                 case "t" -> {
                     //check that arg 1 is a town, if so add it to the town tariff database
-                    townyInstance.getTownyObjectStartingWith(tariffable_entity, "t");
+                   if ( townyInstance.getTownyObjectStartingWith(tariffable_entity, "t").isEmpty()) return; //TODO give command sender message that states town didn't exist
+                    TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), townyInstance.getTown(tariffable_entity).getUUID(), arg_entity_value, tariff_value);
+
+                    SQLHandler.townCreateTariff(DB.get(), tariff);
 
                 }
                 case "n" -> {
                     //check that arg 1 is a nation, if so add it to the town tariff database
-                    townyInstance.getTownyObjectStartingWith(tariffable_entity, "n");
+                    if (townyInstance.getTownyObjectStartingWith(tariffable_entity, "n").isEmpty()) return; //TODO give command sender message that states town didn't exist
+                    TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), townyInstance.getNation(tariffable_entity).getUUID(), arg_entity_value, tariff_value);
+
+                    SQLHandler.townCreateTariff(DB.get(), tariff);
 
                 }
             }
@@ -92,31 +111,57 @@ public class Tariff {
 
     }
     private void ntariff(CommandSender sender, CommandArguments args) {
+        if( sender == null ){
+            return;
+        }
         List<String> entity_value = new ArrayList<>() ;
         entity_value.add("p"); entity_value.add("n"); entity_value.add("t");
-        String arg_entity_value = args.get("n_entity_value") != null ? args.get("n_entity_value").toString(): null;
+        String arg_entity_value = args.get("t_entity_value") != null ? args.get("t_entity_value").toString(): null;
         if (!entity_value.contains(arg_entity_value) || arg_entity_value == null ){
             //Exit Command, first arg isn't p, n, or t
             return;
         }
-        String tariffable_entity = (String) args.get("n_tariffable_entity");
-        switch(arg_entity_value){
-            case "p" -> {
-                //check that arg 1 is a player, if so add it to the town tariff database
-                townyInstance.getTownyObjectStartingWith(tariffable_entity, "r");
+        String tariffable_entity = (String) args.get("t_tariffable_entity");
+        if(args.count() < 3) return;
+        int tariff_value = (int) args.get("t_tariff_value");
 
-            }
-            case "t" -> {
-                //check that arg 1 is a town, if so add it to the town tariff database
-                townyInstance.getTownyObjectStartingWith(tariffable_entity, "t");
+        try {
+            switch (arg_entity_value) {
+                case "p" -> {
+                    //check that arg 1 is a player, if so add it to the town tariff database
+                    townyInstance.getTownyObjectStartingWith(tariffable_entity, "r");
+                    if(Bukkit.getPlayerUniqueId(tariffable_entity)==null) return; //TODO give command sender message that states player didn't exist
+                    TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), Bukkit.getPlayerUniqueId(tariffable_entity), arg_entity_value, tariff_value);
 
-            }
-            case "n" -> {
-                //check that arg 1 is a nation, if so add it to the town tariff database
-                townyInstance.getTownyObjectStartingWith(tariffable_entity, "n");
+                    SQLHandler.nationCreateTariff(DB.get(), tariff);
+                }
+                case "t" -> {
+                    //check that arg 1 is a town, if so add it to the town tariff database
+                    if ( townyInstance.getTownyObjectStartingWith(tariffable_entity, "t").isEmpty()) return; //TODO give command sender message that states town didn't exist
+                    TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), townyInstance.getTown(tariffable_entity).getUUID(), arg_entity_value, tariff_value);
 
+                    SQLHandler.nationCreateTariff(DB.get(), tariff);
+
+                }
+                case "n" -> {
+                    //check that arg 1 is a nation, if so add it to the town tariff database
+                    if (townyInstance.getTownyObjectStartingWith(tariffable_entity, "n").isEmpty()) return; //TODO give command sender message that states town didn't exist
+                    TariffObject tariff = new TariffObject(townyInstance.getTown((Player) sender).getUUID(), townyInstance.getNation(tariffable_entity).getUUID(), arg_entity_value, tariff_value);
+
+                    SQLHandler.nationCreateTariff(DB.get(), tariff);
+
+                }
             }
+        } catch(SQLException e){
+            e.printStackTrace();
         }
 
+
+    }
+
+    private void embargo(CommandSender sender, CommandArguments commandArguments){
+        Object[] argarray = new Object[commandArguments.count() + 1];
+        argarray[argarray.length-1] = -1;
+        CommandArguments passedArgs = new CommandArguments()
     }
 }
